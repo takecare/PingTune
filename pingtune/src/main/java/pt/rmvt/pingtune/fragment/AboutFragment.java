ba209fcd6dc68780c0e30ab765e4e18b0828c051
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 23:01:43 +0100

    basic setup for about fragment

--- pingtune/src/main/java/pt/rmvt/pingtune/fragment/AboutFragment.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/AboutFragment.java
@@ -6,10 +6,45 @@
  */
 package pt.rmvt.pingtune.fragment;
 
-import android.support.v4.app.Fragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class AboutFragment extends Fragment {
+import pt.rmvt.pingtune.R;
+
+public class AboutFragment extends BaseFragment {
 
     public static final String LOG_TAG = "AboutFragment";
+
+    public AboutFragment() {}
+
+    public static AboutFragment newInstance(Resources resources) {
+
+        AboutFragment fragment = new AboutFragment();
+
+        String title = resources.getString(R.string.aboutFragmentTitle);
+
+        Bundle arguments = new Bundle();
+        arguments.putString(KEY_ARGUMENT_FRAGMENT_TITLE,title);
+        fragment.setArguments(arguments);
+
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        // TODO ...
+        return view;
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        return getArguments().getString(KEY_ARGUMENT_FRAGMENT_TITLE);
+    }
 
 }

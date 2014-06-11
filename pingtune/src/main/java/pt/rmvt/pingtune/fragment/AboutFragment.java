4221a2ea09897155dbd2a8129a01d91d407b10b8
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 22:30:55 +0100

    added about fragment

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/AboutFragment.java
@@ -0,0 +1,13 @@
+/**
+ * @date Jun 11, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.fragment
+ */
+package pt.rmvt.pingtune.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class AboutFragment extends Fragment {
+
+}

b1019439bec7c4be5212c0c1f535268fa980390b
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 23:02:34 +0100

    basic setup for detail fragment

--- pingtune/src/main/java/pt/rmvt/pingtune/fragment/DetailFragment.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/DetailFragment.java
@@ -8,12 +8,17 @@ package pt.rmvt.pingtune.fragment;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import pt.rmvt.pingtune.R;
 
 public class DetailFragment extends BaseFragment {
 
     public static final String LOG_TAG = "DetailFragment";
+
+    public DetailFragment() {}
 
     public static CommitListFragment newInstance(Resources resources) {
 
@@ -26,6 +31,14 @@ public class DetailFragment extends BaseFragment {
         fragment.setArguments(arguments);
 
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        // TODO ...
+        return view;
     }
 
     @Override

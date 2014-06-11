5dbc4d999aa3e4e23d931d01a5a2f5e59c7f3351
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 22:36:33 +0100

    added fragment pager adapter

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/PingTuneFragmentPagerAdapter.java
@@ -0,0 +1,25 @@
+/**
+ * @date Jun 11, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.fragment
+ */
+package pt.rmvt.pingtune.fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PingTuneFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public static final String LOG_TAG = "PingTuneFragmentPagerAdapter";
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}

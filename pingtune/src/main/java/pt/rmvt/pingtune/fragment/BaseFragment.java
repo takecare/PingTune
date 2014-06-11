082e2e0e640494605387602d2a5b4d98e6215f3d
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 22:47:26 +0100

    added base fragment

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+/**
+ * @date Jun 11, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.fragment
+ */
+package pt.rmvt.pingtune.fragment;
+
+public class BaseFragment {
+
+}

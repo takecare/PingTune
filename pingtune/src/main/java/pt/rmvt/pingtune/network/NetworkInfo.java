509534d7e2e885b00df2a1e5ac88162b4057df8b
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 23:18:18 +0100

    adding network info

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/network/NetworkInfo.java
@@ -0,0 +1,16 @@
+/**
+ * @date Jun 11, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.network
+ */
+package pt.rmvt.pingtune.network;
+
+public class NetworkInfo {
+
+    public static final String GITHUB_API_ENDPOINT = "https://api.github.com/";
+    public static final String GITHUB_API_COMMITS_REQUEST_FORMAT = "repos/%s/%s/commits";
+
+
+
+}

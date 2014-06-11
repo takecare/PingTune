682b3277433dd84dba4245861b9a3d4b447fcfe4
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 00:40:57 +0100

    working on custom requests

--- pingtune/src/main/java/pt/rmvt/pingtune/network/requests/CommitRequest.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/network/requests/CommitRequest.java
@@ -6,7 +6,56 @@
  */
 package pt.rmvt.pingtune.network.requests;
 
-public class CommitRequest {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import pt.rmvt.pingtune.model.Commit;
+import pt.rmvt.pingtune.network.NetworkInfo;
+import pt.rmvt.pingtune.network.parser.CommitParser;
+import pt.rmvt.pingtune.network.parser.PingTuneParser;
+
+public class CommitRequest extends PingTuneRequest<Commit,JSONArray> {
+
+    private static final String COMMIT_REQUEST_API_URL =
+            NetworkInfo.GITHUB_API_ENDPOINT + String.format(
+                    NetworkInfo.GITHUB_API_COMMITS_REQUEST_FORMAT,
+                    NetworkInfo.GITHUB_RUBY_USER,
+                    NetworkInfo.GITHUB_RUBY_REPO);
+
+    public CommitRequest() {
+        this(new CommitParser());
+    }
+
+    public CommitRequest(PingTuneParser<Commit,JSONArray> parser) {
+        super(parser);
+        mJsonRequest = new JsonArrayRequest(
+                COMMIT_REQUEST_API_URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonObject) {
+                        mResposeListener.onResponse(mParser.parse(jsonObject));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        if (volleyError.networkResponse != null) {
+                            mErrorListener.onError(
+                                    volleyError.getMessage(),
+                                    volleyError.networkResponse.statusCode);
+                        } else {
+                            mErrorListener.onError(volleyError.getMessage(),-1);
+                        }
+                    }
+                });
+    }
 
 
 
--- pingtune/src/main/java/pt/rmvt/pingtune/network/requests/PingTuneRequest.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/network/requests/PingTuneRequest.java
@@ -6,6 +6,77 @@
  */
 package pt.rmvt.pingtune.network.requests;
 
-public class PingTuneRequest {
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import pt.rmvt.pingtune.network.PingTuneRequestManager;
+import pt.rmvt.pingtune.network.parser.PingTuneParser;
+
+// T: object/model expected on response
+// R: object received from network layer (in our case, volley)
+
+public abstract class PingTuneRequest<T,R> {
+
+    public static final String LOG_TAG = "PingTuneRequest";
+
+    protected JsonArrayRequest mJsonRequest;
+    protected PingTuneResponseListener<T> mResposeListener;
+    protected PingTuneErrorListener mErrorListener;
+    protected PingTuneParser<T,R> mParser;
+
+    // CONSTRUCTORS
+    protected PingTuneRequest(PingTuneResponseListener<T> resposeListener,
+                              PingTuneErrorListener errorListener,
+                              PingTuneParser<T, R> parser) {
+        mResposeListener = resposeListener;
+        mErrorListener = errorListener;
+        mParser = parser;
+    }
+
+    protected PingTuneRequest() {}
+
+    public PingTuneRequest(PingTuneParser<T,R> parser) {
+        mParser = parser;
+    }
+
+    public Request getRequest() {
+        return mJsonRequest;
+    }
+
+    // GETTERS & SETTERS
+    public PingTuneResponseListener<T> getResposeListener() {
+        return mResposeListener;
+    }
+
+    public void setResposeListener(PingTuneResponseListener<T> resposeListener) {
+        mResposeListener = resposeListener;
+    }
+
+    public PingTuneErrorListener getErrorListener() {
+        return mErrorListener;
+    }
+
+    public void setErrorListener(PingTuneErrorListener errorListener) {
+        mErrorListener = errorListener;
+    }
+
+    public PingTuneParser<T, R> getParser() {
+        return mParser;
+    }
+
+    public void setParser(PingTuneParser<T, R> parser) {
+        mParser = parser;
+    }
+
+
+
+    public static interface PingTuneResponseListener<T> {
+        public void onResponse(T obj);
+    }
+
+    public static interface PingTuneErrorListener {
+        public void onError(String errorMessage, int statusCode);
+    }
 
 }

dbdb3d578f04fd2d73dd589e21f52c2c1beac6f3
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 22:32:32 +0100

    added detail fragment

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/DetailFragment.java
@@ -0,0 +1,15 @@
+/**
+ * @date Jun 11, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.fragment
+ */
+package pt.rmvt.pingtune.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class DetailFragment extends Fragment {
+
+    public static final String LOG_TAG = "DetailFragment";
+
+}

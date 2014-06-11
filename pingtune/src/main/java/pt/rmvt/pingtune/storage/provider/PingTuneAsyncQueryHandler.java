fba478ae1beb3e55a712964c960f189ba598299a
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 20:49:56 +0100

    added cookie logic and token identifiers

--- pingtune/src/main/java/pt/rmvt/pingtune/storage/provider/PingTuneAsyncQueryHandler.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/storage/provider/PingTuneAsyncQueryHandler.java
@@ -12,12 +12,18 @@ import android.content.ContentValues;
 import android.net.Uri;
 
 import java.util.HashMap;
+import java.util.Random;
 
 import pt.rmvt.pingtune.dao.IDataAccessObject;
 
 public class PingTuneAsyncQueryHandler extends AsyncQueryHandler {
 
     public static final String LOG_TAG = "PingTuneAsyncQueryHandler";
+
+    private static final int QUERY_TOKEN = 0;
+    private static final int INSERT_TOKEN = 1;
+    private static final int DELETE_TOKEN = 2;
+    private static final int UPDATE_TOKEN = 3;
 
     // stores all of our own dao listeners so when an operation finishes and we're notified here
     // (through our own async query listeners) we can relay that message to the specific dao listener
@@ -35,5 +41,14 @@ public class PingTuneAsyncQueryHandler extends AsyncQueryHandler {
 
     }
 
+    // TODO: startInsert
+
+    // TODO: startUpdate
+
+    // TODO: startDelete
+
+    private String getCookie() {
+        return (new Random()).nextLong() + String.valueOf(System.currentTimeMillis());
+    }
 
 }

8df07ea0df21d53b15f31e8777869629244c670e
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-11 22:51:16 +0100

    added title and newInstance to detail fragment

--- pingtune/src/main/java/pt/rmvt/pingtune/fragment/DetailFragment.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/DetailFragment.java
@@ -6,10 +6,30 @@
  */
 package pt.rmvt.pingtune.fragment;
 
-import android.support.v4.app.Fragment;
+import android.content.res.Resources;
+import android.os.Bundle;
 
-public class DetailFragment extends Fragment {
+import pt.rmvt.pingtune.R;
+
+public class DetailFragment extends BaseFragment {
 
     public static final String LOG_TAG = "DetailFragment";
 
+    public static CommitListFragment newInstance(Resources resources) {
+
+        CommitListFragment fragment = new CommitListFragment();
+
+        String title = resources.getString(R.string.detailFragmentTitle);
+
+        Bundle arguments = new Bundle();
+        arguments.putString(KEY_ARGUMENT_FRAGMENT_TITLE,title);
+        fragment.setArguments(arguments);
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        return getArguments().getString(KEY_ARGUMENT_FRAGMENT_TITLE);
+    }
 }

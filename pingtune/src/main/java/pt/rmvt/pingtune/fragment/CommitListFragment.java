9c6cf0e327f5a5d1aaa35fc574960bbccd9d6c50
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 19:47:55 +0100

    commit list click listener

--- pingtune/src/main/java/pt/rmvt/pingtune/fragment/CommitListFragment.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/fragment/CommitListFragment.java
@@ -6,12 +6,14 @@
  */
 package pt.rmvt.pingtune.fragment;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.squareup.otto.Subscribe;
@@ -23,12 +25,13 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import pt.rmvt.pingtune.R;
+import pt.rmvt.pingtune.activity.DetailActivity;
 import pt.rmvt.pingtune.adapter.AuthorAdapter;
 import pt.rmvt.pingtune.bus.PingTuneBus;
 import pt.rmvt.pingtune.model.Author;
 import pt.rmvt.pingtune.model.Commit;
 
-public class CommitListFragment extends BaseFragment {
+public class CommitListFragment extends BaseFragment implements AdapterView.OnItemClickListener {
 
     public static final String LOG_TAG = "CommitListFragment";
 
@@ -54,13 +57,14 @@ public class CommitListFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        PingTuneBus.getBusInstance().register(this);
+
         View view = inflater.inflate(R.layout.fragment_commit_list, container, false);
         ButterKnife.inject(this, view);
 
         mAuthorAdapter = new AuthorAdapter(getActivity(),new ArrayList<Author>());
         mListView.setAdapter(mAuthorAdapter);
-
-        PingTuneBus.getBusInstance().register(this);
+        mListView.setOnItemClickListener(this);
 
         return view;
     }
@@ -75,9 +79,15 @@ public class CommitListFragment extends BaseFragment {
         Log.d(LOG_TAG,"received update: "+commitsByAuthor.size());
 
         mAuthorAdapter.clear();
-        mAuthorAdapter.addAll(commitsByAuthor.keySet());
+        mAuthorAdapter.addAll(commitsByAuthor.keySet()); // FIXME
         mAuthorAdapter.notifyDataSetChanged();
 
         Log.d(LOG_TAG,"adapter count = "+mAuthorAdapter.getCount());
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Author clickedAuthor = mAuthorAdapter.getItem(position);
+        startActivity(new Intent(getActivity(), DetailActivity.class));
     }
 }

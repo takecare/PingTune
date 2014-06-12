e55d48e476bdf33795a9281a9fad128384499e68
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 22:25:58 +0100

    fixing authors and commits network persistence

--- pingtune/src/main/java/pt/rmvt/pingtune/datamanager/PingTuneDataManager.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/datamanager/PingTuneDataManager.java
@@ -17,6 +17,7 @@ import com.android.volley.toolbox.ImageLoader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import pt.rmvt.pingtune.bus.PingTuneBus;
@@ -59,7 +60,7 @@ public class PingTuneDataManager {
 
     public void update(Context context) {
         if (isNetworkConnectionAvailable(context)) {
-            fetchCommitsFromNetwork(null);
+            fetchCommitsFromNetwork(context,null);
         } else {
             fetchCommitsFromStorage(context);
         }
@@ -67,7 +68,7 @@ public class PingTuneDataManager {
 
     // -
 
-    private void fetchCommitsFromNetwork(String lastSha) {
+    private void fetchCommitsFromNetwork(final Context context, String lastSha) {
         CommitRequest request = lastSha == null ? new CommitRequest() : new CommitRequest(lastSha);
         mRequestManager.executeRequest(
                 request,
@@ -76,10 +77,11 @@ public class PingTuneDataManager {
                     public void onResponse(List<Commit> list) {
                         if (mLastSha == null) {
                             mLastSha = list.get(list.size()-1).getSha();
-                            fetchCommitsFromNetwork(mLastSha);
+                            fetchCommitsFromNetwork(context,mLastSha);
                         }
                         HashMap<Author, List<Commit>> commitsByAuthor = groupCommitsByAuthor(list);
                         mCommitsByAuthor.putAll(commitsByAuthor);
+                        persistCommitsIntoStorage(context,mCommitsByAuthor);
                         PingTuneBus.getBusInstance().post(commitsByAuthor);
                     }
                 },
@@ -98,55 +100,7 @@ public class PingTuneDataManager {
         final HashMap<String,Author> authorsByName = new HashMap<String,Author>();
         final ArrayList<Commit> commits = new ArrayList<Commit>();
 
-        AuthorDAO authorDAO = new AuthorDAO(context.getContentResolver());
-        authorDAO.readAll(new IDataAccessObject.IReadListener<Author>() {
-            @Override
-            public void onReadFinished(Cursor cursor) {
-                if (cursor != null && cursor.getCount() > 0) {
-                    Author author;
-                    AuthorCursor authorCursor;
-                    cursor.moveToFirst();
-                    do {
-                        authorCursor = new AuthorCursor(cursor);
-                        author = new Author();
-                        author.setName(authorCursor.getName());
-                        author.setFollowersUrl(authorCursor.getFollowersurl());
-                        author.setFollowingUrl(authorCursor.getFollowingurl());
-                        author.setStarredUrl(authorCursor.getStarredurl());
-                        author.setTextDate(authorCursor.getDate());
-                        author.setEmail(authorCursor.getEmail());
-                        authorsByName.put(author.getName(),author);
-                    } while (cursor.moveToNext());
-                }
-            }
-        });
-
-        CommitDAO commitDAO = new CommitDAO(context.getContentResolver());
-        commitDAO.readAll(new IDataAccessObject.IReadListener<Commit>() {
-            @Override
-            public void onReadFinished(Cursor cursor) {
-                if (cursor != null && cursor.getCount() > 0) {
-                    Commit commit;
-                    CommitCursor commitCursor;
-                    cursor.moveToFirst();
-                    do {
-                        commitCursor = new CommitCursor(cursor);
-                        commit = new Commit();
-                        commit.setSha(commitCursor.getSha());
-                        commit.setHtmlUrl(commitCursor.getHtmlurl());
-                        commit.setParentSha(commitCursor.getParentsha());
-                        commit.setUrl(commitCursor.getUrl());
-                        commit.setAuthor(authorsByName.get(commitCursor.getAuthorname()));
-                        commits.add(commit);
-                    } while (commitCursor.moveToNext());
-                }
-            }
-        });
-
-        if (commits.size() > 0) {
-            commitsByAuthor = groupCommitsByAuthor(commits);
-            PingTuneBus.getBusInstance().post(commitsByAuthor);
-        }
+        readAuthorsAndCommitsFromStorage(context,authorsByName,commits);
     }
 
     private void persistCommitsIntoStorage(Context context,
@@ -193,5 +147,68 @@ public class PingTuneDataManager {
         }
 
         return false;
+    }
+
+
+
+    private void readAuthorsAndCommitsFromStorage(final Context context,
+                                                  final Map<String,Author> authorsByName,
+                                                  final List<Commit> commits) {
+        AuthorDAO authorDAO = new AuthorDAO(context.getContentResolver());
+        authorDAO.readAll(new IDataAccessObject.IReadListener<Author>() {
+            @Override
+            public void onReadFinished(Cursor cursor) {
+                if (cursor != null && cursor.getCount() > 0) {
+                    Author author;
+                    AuthorCursor authorCursor;
+                    cursor.moveToFirst();
+                    do {
+                        authorCursor = new AuthorCursor(cursor);
+                        author = new Author();
+                        author.setName(authorCursor.getName());
+                        author.setFollowersUrl(authorCursor.getFollowersurl());
+                        author.setFollowingUrl(authorCursor.getFollowingurl());
+                        author.setStarredUrl(authorCursor.getStarredurl());
+                        author.setTextDate(authorCursor.getDate());
+                        author.setEmail(authorCursor.getEmail());
+                        authorsByName.put(author.getName(),author);
+                    } while (cursor.moveToNext());
+                    cursor.close();
+                    readCommitsFromStorageAndPost(context,authorsByName,commits);
+                }
+            }
+        });
+    }
+
+    private void readCommitsFromStorageAndPost(Context context,
+                                               final Map<String,Author> authorsByName,
+                                               final List<Commit> commits) {
+        CommitDAO commitDAO = new CommitDAO(context.getContentResolver());
+        commitDAO.readAll(new IDataAccessObject.IReadListener<Commit>() {
+            @Override
+            public void onReadFinished(Cursor cursor) {
+                if (cursor != null && cursor.getCount() > 0) {
+                    Commit commit;
+                    CommitCursor commitCursor;
+                    cursor.moveToFirst();
+                    do {
+                        commitCursor = new CommitCursor(cursor);
+                        commit = new Commit();
+                        commit.setSha(commitCursor.getSha());
+                        commit.setHtmlUrl(commitCursor.getHtmlurl());
+                        commit.setParentSha(commitCursor.getParentsha());
+                        commit.setUrl(commitCursor.getUrl());
+                        commit.setAuthor(authorsByName.get(commitCursor.getAuthorname()));
+                        commits.add(commit);
+                    } while (commitCursor.moveToNext());
+                    cursor.close();
+
+                    Log.d(LOG_TAG,"#readCommitsFromStorageAndPost: "+commits.size());
+                    if (commits.size() > 0) {
+                        PingTuneBus.getBusInstance().post(groupCommitsByAuthor(commits));
+                    }
+                }
+            }
+        });
     }
 }

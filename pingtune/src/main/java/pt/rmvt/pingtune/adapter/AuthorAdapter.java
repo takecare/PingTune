d86c3c5cb3cf433be3feb275319e798e195d9899
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 02:41:52 +0100

    new author row layout

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/adapter/AuthorAdapter.java
@@ -0,0 +1,11 @@
+/**
+ * @date Jun 12, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.adapter
+ */
+package pt.rmvt.pingtune.adapter;
+
+public class AuthorAdapter {
+
+}

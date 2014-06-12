71df12c8d2236bab8892249664d606bae52c5cde
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 13:12:33 +0100

    developing following cardinality request

--- pingtune/src/main/java/pt/rmvt/pingtune/network/requests/FollowingCardinalityRequest.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/network/requests/FollowingCardinalityRequest.java
@@ -6,22 +6,51 @@
  */
 package pt.rmvt.pingtune.network.requests;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
 import org.json.JSONArray;
 
-import java.util.List;
-
-import pt.rmvt.pingtune.model.Commit;
 import pt.rmvt.pingtune.network.parser.PingTuneParser;
 
-public class FollowingCardinalityRequest extends PingTuneRequest<List<Commit>,JSONArray> {
+public class FollowingCardinalityRequest extends PingTuneRequest<Integer,JSONArray> {
 
     public static final String LOG_TAG = "FollowingRequest";
 
     private static String sFOLLOWING_REQUEST_API_URL;
 
+    public FollowingCardinalityRequest(String url) {
+        sFOLLOWING_REQUEST_API_URL = cleanUpUrl(url);
+    }
 
-    private FollowingCardinalityRequest(PingTuneParser<Integer, JSONArray> parser) {
+    private FollowingCardinalityRequest(PingTuneParser<Integer,JSONArray> parser) {
+        super(parser);
+        mJsonRequest = new JsonArrayRequest(
+                sFOLLOWING_REQUEST_API_URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        mResposeListener.onResponse(mParser.parse(jsonArray));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        if (volleyError.networkResponse != null) {
+                            mErrorListener.onError(
+                                    volleyError.getMessage(),
+                                    volleyError.networkResponse.statusCode);
+                        } else {
+                            mErrorListener.onError(volleyError.getMessage(),-1);
+                        }
+                    }
+                });
+    }
 
+    private String cleanUpUrl(String url) {
+        // TODO: remove everything enclosed in {}
+        return url;
     }
 
 }

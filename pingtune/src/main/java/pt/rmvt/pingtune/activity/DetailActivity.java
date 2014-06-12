452c88d9853e52f5daa9759a2ee6a9e6278e2918
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 19:46:55 +0100

    added detail activity

---
+++ pingtune/src/main/java/pt/rmvt/pingtune/activity/DetailActivity.java
@@ -0,0 +1,11 @@
+/**
+ * @date Jun 12, 2014
+ * @author Rui Teixeira, rui@vazteixeira.org
+ * @copyright Copyright 2014 Rui Teixeira. All rights reserved.
+ * PingTune - pt.rmvt.pingtune.activity
+ */
+package pt.rmvt.pingtune.activity;
+
+public class DetailActivity {
+
+}

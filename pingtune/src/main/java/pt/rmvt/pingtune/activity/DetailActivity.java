e3e87382a68f1d768dd457ad730f8bf7259557a0
Rui Teixeira <rui.teixeira@blip.pt>
2014-06-12 19:47:20 +0100

    detail activity oncreate

--- pingtune/src/main/java/pt/rmvt/pingtune/activity/DetailActivity.java
+++ pingtune/src/main/java/pt/rmvt/pingtune/activity/DetailActivity.java
@@ -6,6 +6,32 @@
  */
 package pt.rmvt.pingtune.activity;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+
+import butterknife.ButterKnife;
+import pt.rmvt.pingtune.R;
+
+public class DetailActivity extends ActionBarActivity {
+
+    public static final String LOG_TAG = "DetailActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        // actionbar config
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
 
 }
